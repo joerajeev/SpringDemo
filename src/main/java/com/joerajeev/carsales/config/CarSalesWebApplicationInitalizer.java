df58eef22d5887fb07ba17b70f44db80f56ba65b
Rajeev <joerajeev.eng@gmail.com>
2015-01-29 07:11:27 +1100

    Converted to pure code based configuration

--- src/main/java/com/joerajeev/carsales/config/CarSalesWebApplicationInitalizer.java
+++ src/main/java/com/joerajeev/carsales/config/CarSalesWebApplicationInitalizer.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -25,32 +26,34 @@ public class CarSalesWebApplicationInitalizer implements WebApplicationInitializ
 	public void onStartup(ServletContext container) throws ServletException {
 		
 		/*
-		 * Loading application contexts from the Annotation Config classes
-		 * Alternatively, we could have extended the AbstractAnnotationConfigDispatcherServletInitializer and overridden 
-		 * the getRootConfigClasses()
-		 */
-		AnnotationConfigWebApplicationContext annoAppCtx = new AnnotationConfigWebApplicationContext();
-		annoAppCtx.register(AppConfig.class, WebSecurityConfig.class);
-		container.addListener(new ContextLoaderListener(annoAppCtx));
-				
-		//Replaced the below xml based config by the Annotation based config (above)
-		/*XmlWebApplicationContext appCtx = new XmlWebApplicationContext();
-		appCtx.setConfigLocation("classpath:com/joerajeev/carsales/config/app-context.xml");
-		container.addListener(new ContextLoaderListener(appCtx));*/
-		
-		/*
 		 * Adding the springSecurityFilterChain filter to the servlet context
 		 * Alternatively could have extended the AbstractSecurityWebApplicationInitializer  
 		 */
 		FilterRegistration.Dynamic filterReg = container.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
 		filterReg.addMappingForUrlPatterns(null, true, "/*");
 		
+		/*
+		 * Loading application contexts from the Annotation Config classes
+		 * Alternatively, we could have extended the AbstractAnnotationConfigDispatcherServletInitializer and overridden 
+		 * the getRootConfigClasses() / getServletConfigClasses()
+		 */
+		AnnotationConfigWebApplicationContext annoAppCtx = new AnnotationConfigWebApplicationContext();
+		annoAppCtx.register(AppConfig.class, WebSecurityConfig.class);
+		container.addListener(new ContextLoaderListener(annoAppCtx));
+				
+		//Replaced the below xml based config by the Annotation based config 
+		/*XmlWebApplicationContext appCtx = new XmlWebApplicationContext();
+		appCtx.setConfigLocation("classpath:com/joerajeev/carsales/config/app-context.xml");
+		container.addListener(new ContextLoaderListener(appCtx));
 		
 		XmlWebApplicationContext dispatcherContext = new XmlWebApplicationContext();
-		dispatcherContext.setConfigLocation("/WEB-INF/carsales-servlet.xml");
+		dispatcherContext.setConfigLocation("/WEB-INF/carsales-servlet.xml");*/
+		
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.register(WebConfig.class);
 
 		ServletRegistration.Dynamic dispatcher = container.addServlet(
-				"dispatcher", new DispatcherServlet(dispatcherContext));
+				"dispatcher", new DispatcherServlet(webContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
 	}
